90e6e1c28f1cb62a2b3ea815894359bb834a02d8
Connor Shugg <cwshugg@vt.edu>
2022-05-13 11:51:17 -0400

    fixed conflicting JWT library issue

--- scripts/get-dependencies.sh
+++ scripts/get-dependencies.sh
@@ -12,7 +12,9 @@ ${pip} install --user -U pip setuptools
 echo -e "${C_ACC}Installing flask...${C_NONE}"
 ${pip} install --user -U Flask
 echo -e "${C_ACC}Installing jwt...${C_NONE}"
-${pip} install --user -U jwt
+${pip} uninstall --user -U JWT
+${pip} uninstall --user -U PyJWT
+${pip} install --user -U PyJWT
 echo -e "${C_ACC}Installing openpyxl...${C_NONE}"
 ${pip} install --user -U openpyxl
 
